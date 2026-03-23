58f8679ab1e2f4ece14a8be6c2895b28526b3945
Lucas Zanoni <lucas.zanoni@coatesgroup.com>
2026-03-23 08:59:14 -0300

    feat(karabiner): remap Ctrl+Click to Cmd+Click except in terminals

--- home/modules/desktop/karabiner.nix
+++ home/modules/desktop/karabiner.nix
@@ -232,6 +232,28 @@ let
                 ];
           }
           {
+            description = "Ctrl+Click to Cmd+Click (except in terminals)";
+            manipulators = [
+              {
+                type = "basic";
+                from = {
+                  pointing_button = "button1";
+                  modifiers = {
+                    mandatory = [ "control" ];
+                    optional = [ "any" ];
+                  };
+                };
+                to = [
+                  {
+                    pointing_button = "button1";
+                    modifiers = [ "command" ];
+                  }
+                ];
+                conditions = excludeTerminalsCondition;
+              }
+            ];
+          }
+          {
             description = "Linux-style Ctrl to Cmd shortcuts (except in terminals)";
             manipulators = map (
               letter: makeControlToCommandManipulator letter (if letter == "w" then "q" else letter)
