74a8ef6d1e32880507e251ec32b0a34df34e2756
Lucas Zanoni <lucas.zanoni@coatesgroup.com>
2026-03-23 09:48:19 -0300

    fix(karabiner): remove Ctrl+V from Ctrl-to-Cmd remap so it does not trigger Maccy
    Maccy stays on Cmd+V. Ctrl+V no longer remaps to Cmd+V in non-terminal
    apps, keeping the two shortcuts independent.

--- home/modules/desktop/maccy.nix
+++ home/modules/desktop/maccy.nix
@@ -3,7 +3,7 @@
   home.packages = [ pkgs.maccy ];
 
   home.activation.configureMaccyDefaults = config.lib.dag.entryAfter [ "writeBoundary" ] ''
-    /usr/bin/defaults write org.p0deje.Maccy KeyboardShortcuts_popup -string '{"carbonKeyCode":9,"carbonModifiers":768}'
+    /usr/bin/defaults write org.p0deje.Maccy KeyboardShortcuts_popup -string '{"carbonKeyCode":9,"carbonModifiers":256}'
     /usr/bin/defaults write org.p0deje.Maccy pasteByDefault -bool true
     /usr/bin/defaults write org.p0deje.Maccy SUEnableAutomaticChecks -bool false
     /usr/bin/defaults write org.p0deje.Maccy loginItemEnabled -bool true
