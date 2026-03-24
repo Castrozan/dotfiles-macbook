9263388a9b0d8ef226ad26815b3e4d141fd3ddeb
Lucas Zanoni <lucas.zanoni@coatesgroup.com>
2026-03-24 18:14:27 -0300

    feat(session)(lucas.zanoni): add system PATH entries to home.sessionPath for all shells

--- users/lucas.zanoni/home/session-vars.nix
+++ users/lucas.zanoni/home/session-vars.nix
@@ -1,4 +1,11 @@
 _: {
+  home.sessionPath = [
+    "/run/current-system/sw/bin"
+    "/nix/var/nix/profiles/default/bin"
+    "/usr/bin"
+    "/bin"
+  ];
+
   home.sessionVariables = {
     OBSIDIAN_HOME = "$HOME/vault";
     EDITOR = "code";
