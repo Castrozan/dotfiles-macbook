6fb57aec1cb4714cb1b622bdd8b3c491fff2453b
Lucas Zanoni <lucas.zanoni@coatesgroup.com>
2026-04-18 14:55:00 -0300

    fix(screensaver): add shellcheck directive to disable SC1090

--- home/modules/terminal/shell/screensaver.sh
+++ home/modules/terminal/shell/screensaver.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # shellcheck disable=SC1091
+# shellcheck disable=SC1090
 source ~/.dotfiles/home/modules/terminal/shell/screensaver/config.sh
 # shellcheck disable=SC1091
 source ~/.dotfiles/home/modules/terminal/shell/screensaver/core.sh
